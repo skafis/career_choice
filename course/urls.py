56670ba8815468e684ea9ac3749b90a5de8d16ec
frank <franciskipchumba5@gmail.com>
2016-12-22 15:38:57 +0300

    list all courses

--- course/urls.py
+++ course/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'^$', views.course_list, name='course_list')
 ]
--- course/views.py
+++ course/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Course_instructor, Courses, Learners
 
 # Create your views here.
+def course_list(request):
+	queryset = Courses.objects.all()
+
+	context = {
+		'queryset':queryset
+	}
+	return render(request, 'index.html', context)
